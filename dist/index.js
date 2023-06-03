4cdc47cc0fe0c81cfe244f312502a967d9c3a422
Subham Beura <subhambeura20@gmail.com>
2023-06-03 20:17:01 +0530

    connected To DB

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4000;
+try {
+    const dbURL = process.env.DB_URL;
+    mongoose_1.default.connect(dbURL);
+    console.log("DB Connected");
+}
+catch (error) {
+    console.log("DB Connection Error");
+}
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Express, Response, Request } from "express";
 import dotenv from "dotenv";
+import mongoose, { mongo } from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
+
+try {
+  const dbURL = process.env.DB_URL as string;
+  mongoose.connect(dbURL);
+  console.log("DB Connected");
+} catch (error) {
+  console.log("DB Connection Error");
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
