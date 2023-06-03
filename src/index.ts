0bacd6df8a1ddc111f6359ca8862790716446d01
Subham Beura <subhambeura20@gmail.com>
2023-06-03 19:43:48 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Response, Request } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => console.log("Server is on!!"));
