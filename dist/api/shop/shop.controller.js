e16906a6131d53e0d177d3ed8220a01074eb3905
Subham Beura <subhambeura20@gmail.com>
2023-07-23 18:33:14 +0530

    set up shop controllers

--- dist/api/shop/shop.controller.js
+++ dist/api/shop/shop.controller.js
@@ -10,11 +10,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteShop = exports.updateShop = exports.createShop = exports.getShopById = exports.getAllShops = void 0;
-const getAllShops = (req, res) => __awaiter(void 0, void 0, void 0, function* () { });
+const users_controller_1 = require("../users/users.controller");
+const getAllShops = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const shops = yield users_controller_1.prisma.shop.findMany();
+    res.json(shops);
+});
 exports.getAllShops = getAllShops;
-const getShopById = (req, res) => __awaiter(void 0, void 0, void 0, function* () { });
+const getShopById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const shop = yield users_controller_1.prisma.shop.findUnique({
+        where: {
+            id: String(id),
+        },
+    });
+    res.json(shop);
+});
 exports.getShopById = getShopById;
-const createShop = (req, res) => __awaiter(void 0, void 0, void 0, function* () { });
+const createShop = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const shop = yield users_controller_1.prisma.shop.create({
+        data: Object.assign({}, req.body),
+    });
+    res.status(201).json(shop);
+});
 exports.createShop = createShop;
 const updateShop = (req, res) => __awaiter(void 0, void 0, void 0, function* () { });
 exports.updateShop = updateShop;
--- src/api/shop/shop.controller.ts
+++ src/api/shop/shop.controller.ts
@@ -1,8 +1,61 @@
 import { Request, Response } from "express";
 import { prisma } from "../users/users.controller";
 
-export const getAllShops = async (req: Request, res: Response) => {};
-export const getShopById = async (req: Request, res: Response) => {};
-export const createShop = async (req: Request, res: Response) => {};
-export const updateShop = async (req: Request, res: Response) => {};
-export const deleteShop = async (req: Request, res: Response) => {};
+export const getAllShops = async (req: Request, res: Response) => {
+  const shops = await prisma.shop.findMany();
+  res.json(shops);
+};
+export const getShopById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const shop = await prisma.shop.findUnique({
+    where: {
+      id: String(id),
+    },
+  });
+  if (!shop) return res.status(404).json({ error: "Shop not found" });
+  res.status(200).json(shop);
+};
+export const createShop = async (req: Request, res: Response) => {
+  try {
+    const shop = await prisma.shop.create({
+      data: {
+        ...req.body,
+      },
+    });
+    res.status(201).json(shop);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+export const updateShop = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const shop = await prisma.shop.update({
+      where: {
+        id: String(id),
+      },
+      data: {
+        ...req.body,
+      },
+    });
+    if (!shop) return res.status(404).json({ error: "Shop not found" });
+    res.status(200).json(shop);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+export const deleteShop = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const shop = await prisma.shop.delete({
+      where: {
+        id: String(id),
+      },
+    });
+    if (!shop) return res.status(404).json({ error: "Shop not found" });
+    res.status(200).json(shop);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
